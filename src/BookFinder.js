628c9b5bf6588b22e15b69127a9d1707275ed322
Serena Lertkantitham <serena.lertkantitham@gmail.com>
2019-09-12 21:06:10 -0700

    loading & no results found states

--- src/BookFinder.js
+++ src/BookFinder.js
@@ -11,11 +11,16 @@ import { GBOOK_KEY } from "./config";
 export default class BookFinder extends React.Component {
   state = {
     searchQuery: "",
+    isLoading: false,
+    searchExecuted: false,
     bookData: []
   };
 
   getBookDataFromAPI = e => {
     e.preventDefault();
+
+    // display loading page
+    this.setState({ isLoading: true, searchExecuted: false, dataFound: false });
 
     // get search text string
     let API_URL =
@@ -32,7 +37,13 @@ export default class BookFinder extends React.Component {
           return res.json();
         }
       })
-      .then(resj => this.setState({ bookData: resj.items }))
+      .then(resj => {
+        this.setState({
+          isLoading: false,
+          searchExecuted: true,
+          bookData: resj.items
+        });
+      })
       .catch(err =>
         alert(`Something may have gone wrong! Try again. Ref: ${err}`)
       );
@@ -53,9 +64,20 @@ export default class BookFinder extends React.Component {
           handleSubmit={e => this.getBookDataFromAPI(e)}
         />
         <div id="book-card-container">
-          {this.state.bookData.map(book => (
-            <BookCard volumeInfo={book.volumeInfo} />
-          ))}
+          {this.state.isLoading && <h2>Finding books...</h2>}
+
+          {this.state.searchExecuted &&
+            this.state.bookData &&
+            this.state.bookData.map(book => (
+              <BookCard volumeInfo={book.volumeInfo} />
+            ))}
+
+          {/* this.state.bookData.map(book => (
+              <BookCard volumeInfo={book.volumeInfo} /> */}
+
+          {this.state.searchExecuted && !this.state.bookData && (
+            <h2>No results found. Try a different search term!</h2>
+          )}
         </div>
       </React.Fragment>
     );
